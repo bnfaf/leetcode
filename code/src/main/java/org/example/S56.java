426df1d44b38bb32b52f43f1ca0b7f75d7f2b1b6
10840 <1084083934@qq.com>
2023-02-25 21:11:51 +0800

    leetcode 刷题2023.2.09

--- code/src/main/java/org/example/S56.java
+++ code/src/main/java/org/example/S56.java
@@ -7,23 +7,29 @@ import java.util.List;
 
 public class S56 {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 0){
+        if (intervals.length < 1){
             return new int[0][2];
         }
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] interval1, int[] interval2){
-                return interval1[0] - interval2[0];
+        Arrays.sort(intervals, (a, b)  -> {
+            if (a[0] == b[0]){
+                return Integer.compare(a[1], b[1]);
+            }else{
+                return Integer.compare(a[0], b[0]);
             }
         });
+        int L = intervals[0][0], R = intervals[0][1];
         List<int[]> merged = new ArrayList<int[]>();
-        for (int i = 0; i < intervals.length; i++) {
-            int L = intervals[i][0], R = intervals[i][1];
-            if (merged.size() == 0 || merged.get(merged.size()-1)[1] < L){
-                merged.add(new int[] {L, R});
-            }else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+        for (int i = 1; i < intervals.length; i++) {
+            if (R >= intervals[i][0]){
+                R = Math.max(intervals[i][1], R);
+            }else{
+                merged.add(new int[]{L, R});
+                L = intervals[i][0];
+                R = intervals[i][1];
             }
         }
+        merged.add(new int[]{L, R});
         return merged.toArray(new int[merged.size()][]);
+
     }
 }
