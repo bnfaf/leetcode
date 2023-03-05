90263ea8dc97cc3e3bc4612a5fd95ed76a50ad2b
10840 <1084083934@qq.com>
2023-03-05 19:56:33 +0800

    leetcode 刷题2023.2.09

--- code/src/main/java/org/example/S300.java
+++ code/src/main/java/org/example/S300.java
@@ -1,38 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class S300 {
     public int lengthOfLIS(int[] nums) {
-        int len = 1, n = nums.length;
-        if (n == 0) {
-            return 0;
-        }
-        int[] d = new int[n+1];
-        d[len] = nums[0];
-        for (int i = 1; i < n; i++) {
-            if(nums[i] > d[len]){
-                d[++len] = nums[i];
-            }else {
-                int l = 1, r = len, pos = 0;
-                while (l <= r) {
-                    int mid = (l + r) >> 1;
-                    if (d[mid] < nums[i]){
-                        pos = mid;
-                        l = mid + 1;
-                    }else {
-                        r = mid - 1;
-                    }
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
-                d[pos+1] = nums[i];
             }
+            res = Math.max(res, dp[i]);
         }
-        return len;
+        return res;
     }
 
     public static void main(String[] args) {
         S300 solution = new S300();
-        int[] nums = {10,9,2,5,3, 1,7,101,18};
+        int[] nums = {0,1,0,3,2,3};
         System.out.println(solution.lengthOfLIS(nums));
     }
 }
